1aed68d3f10e549c5c94341894e9300e5f8aed6f
Artem Lysevych <alysevyc@mion.elka.pw.edu.pl>
2020-07-18 22:39:31 +0200

    Changed repo names and README

--- Pokemon Cover Flow/GestureRecognizers/CatchGestureRecognizer.swift
+++ Pokemon Cover Flow/GestureRecognizers/CatchGestureRecognizer.swift
@@ -2,7 +2,7 @@
 //  MyTapGestureRecognizer.swift
 //  Pokemon Cover Flow
 //
-//  Created by Taras Kulyavets on 02.05.2020.
+//  Created by Best Mac on 02.05.2020.
 //  Copyright © 2020 DaftAcademy. All rights reserved.
 //
 
